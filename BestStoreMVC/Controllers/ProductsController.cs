aa7742a6e21aa1f7b69b049a08ecabbb393f8429
Ivana <ivanajankovic1302@gmail.com>
2025-03-20 19:15:22 +0100

    initial commit

---
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDBContext context;
+        public ProductsController(ApplicationDBContext context)
+        {
+            this.context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p=>p.Id).ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BestStoreMVC/Services/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
